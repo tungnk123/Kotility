c620940e6bb3ce2d7dfa395764f393605d38bc4a
tungnk123 <doanthanhtungnk123@gmail.com>
2025-07-28 23:35:56 +0700

    Add toast extensions

---
+++ toast/src/main/java/com/tungnk123/toast/ToastExtensions.kt
@@ -0,0 +1,18 @@
+package com.tungnk123.toast
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+
+fun Context.showToast(@StringRes resId: Int, isLong: Boolean = false) {
+    Toast.makeText(this, resId, if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showToast(message: CharSequence, isLong: Boolean = false) {
+    Toast.makeText(this, message, if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.showToast(@StringRes resId: Int, isLong: Boolean = false) = requireContext().showToast(resId, isLong)
+
+fun Fragment.showToast(message: CharSequence, isLong: Boolean = false) = requireContext().showToast(message, isLong)
